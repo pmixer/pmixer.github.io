b985bf2b7e149fb6b66d86c8cc894f97ef08c364
peterhuang2015 <dreaming_hz@hotmail.com>
2019-08-26 11:24:09 +0800

    add more countdown

--- js/countdown.js
+++ js/countdown.js
@@ -18,6 +18,112 @@ website: "https://aaai.org/Conferences/AAAI-20/aaai20call/",
 approx: 0,
 });
 
+deadlines.push({
+venue: "ICWSM-2020",
+area: "Web and Social Media",
+deadline: moment("2020-01-15 23:59:00 -1200", "YYYY-MM-DD HH:mm:ss Z"),
+website: "https://www.icwsm.org/2020/index.html",
+approx: 0,
+});
+
+deadlines.push({
+venue: "ACL-2020",
+area: "Computational Linguistics",
+deadline: moment("2019-12-09 23:59:00 -1200", "YYYY-MM-DD HH:mm:ss Z"),
+website: "https://acl2020.org/",
+approx: 0,
+});
+
+deadlines.push({
+venue: "IJCAI-2020",
+area: "Artifical Intelligence",
+deadline: moment("2020-01-01 23:59:00 -1200", "YYYY-MM-DD HH:mm:ss Z"),
+website: "https://ijcai20.org/",
+approx: 1,
+});
+
+deadlines.push({
+venue: "WWW-2020",
+area: "The Web Conference",
+deadline: moment("2020-01-01 23:59:00 -1200", "YYYY-MM-DD HH:mm:ss Z"),
+website: "https://www2020.thewebconf.org/",
+approx: 1,
+});
+
+deadlines.push({
+venue: "CVPR-2020",
+area: "Computer Vision and Pattern Recognition",
+deadline: moment("2019-11-15 23:59:00 -0800", "YYYY-MM-DD HH:mm:ss Z"),
+website: "http://cvpr2020.thecvf.com/",
+approx: 0,
+});
+
+deadlines.push({
+venue: "WACV-2020",
+area: "Computer Vision",
+deadline: moment("2019-10-11 23:59:00 -0800", "YYYY-MM-DD HH:mm:ss Z"),
+website: "https://wacv20.wacv.net/index.php/dates/",
+approx: 0,
+});
+
+deadlines.push({
+venue: "ICRA-2020",
+area: "Robotics",
+deadline: moment("2019-09-10 23:59:00 -0800", "YYYY-MM-DD HH:mm:ss Z"),
+website: "http://www.icra2020.org/call-for-papers",
+approx: 0,
+});
+
+deadlines.push({
+venue: "MICCAI-2020",
+area: "Medical Imaging",
+deadline: moment("2020-04-01 23:59:00 -0800", "YYYY-MM-DD HH:mm:ss Z"),
+website: "http://www.miccai.org/events/upcoming-conferences/",
+approx: 1,
+});
+
+deadlines.push({
+venue: "ACM MM-2020",
+area: "Multimedia",
+deadline: moment("2020-03-31 23:59:00 -0800", "YYYY-MM-DD HH:mm:ss Z"),
+website: "https://acmmm.org/",
+approx: 1,
+});
+
+deadlines.push({
+venue: "ICASSP-2020",
+area: "Signal Processing",
+deadline: moment("2019-10-21 23:59:00 -0000", "YYYY-MM-DD HH:mm:ss Z"),
+website: "https://2020.ieeeicassp.org/authors/call-for-papers/",
+approx: 1,
+});
+
+deadlines.push({
+venue: "ICML-2020",
+area: "Machine Learning",
+deadline: moment("2020-01-01 23:59:00 -0000", "YYYY-MM-DD HH:mm:ss Z"),
+website: "https://icml.cc/Conferences/2020",
+approx: 1,
+});
+
+deadlines.push({
+venue: "KDD-2020",
+area: "Knowledge Discovery and Data Mining",
+deadline: moment("2020-02-01 23:59:00 -0000", "YYYY-MM-DD HH:mm:ss Z"),
+website: "https://www.kdd.org/kdd2020/",
+approx: 1,
+});
+
+deadlines.push({
+venue: "More",
+area: "By jackietseng",
+deadline: moment("2020-12-31 23:59:00 -0000", "YYYY-MM-DD HH:mm:ss Z"),
+website: "https://jackietseng.github.io/conference_call_for_paper/conferences.html",
+approx: 1,
+});
+
+
+
 // HELPER FUNCTIONS
 var timeDescription = function(x) {
 return x.format("MM/DD/YYYY h:mm:ss A");
